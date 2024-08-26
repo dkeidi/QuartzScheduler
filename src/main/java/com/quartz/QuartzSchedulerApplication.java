4590afa0519ec050587a0fc7950923da7b748d6b
Keidi Tay <keidizq@gmail.com>
2024-08-26 21:12:38 +0800

    revert jobpropertiesloader

--- src/main/java/com/quartz/QuartzSchedulerApplication.java
+++ src/main/java/com/quartz/QuartzSchedulerApplication.java
@@ -1,6 +1,7 @@
 package com.quartz;
 
 import com.quartz.jobs.BatchJob;
+import com.quartz.util.JobPropertiesLoader;
 import com.quartz.util.Log4j2XmlGenerator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
---
+++ src/main/java/com/quartz/util/JobPropertiesLoader.java
@@ -0,0 +1,17 @@
+package com.quartz.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class JobPropertiesLoader {
+    public static Properties loadJobProperties(String filePath) throws IOException {
+        Properties properties = new Properties();
+        InputStream inStream = Files.newInputStream(Paths.get(filePath));
+        properties.load(inStream);
+        inStream.close();
+        return properties;
+    }
+}
