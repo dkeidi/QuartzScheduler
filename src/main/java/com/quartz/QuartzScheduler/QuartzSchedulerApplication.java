9b6d1b1d4cf683b7fc35ef5990f1a5a18b39208f
Keidi Tay <keidizq@gmail.com>
2024-10-08 16:58:10 +0800

    remove conflicting declaration to solve main class issue

--- src/main/java/com/quartz/QuartzScheduler/QuartzSchedulerApplication.java
+++ src/main/java/com/quartz/QuartzScheduler/QuartzSchedulerApplication.java
@@ -1,54 +1,161 @@
 package com.quartz.QuartzScheduler;
 
-import com.quartz.QuartzScheduler.model.User;
 import com.quartz.QuartzScheduler.repo.UserRepo;
+import com.quartz.QuartzScheduler.jobs.BatchJob;
+import com.quartz.QuartzScheduler.util.JobPropertiesLoader;
+import com.quartz.QuartzScheduler.util.Log4j2XmlGenerator;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 @SpringBootApplication
 public class QuartzSchedulerApplication {
 
-//    @Autowired
-//    private JdbcTemplate jdbcTemplate;
-//    private static Logger LOG;
+    private static Logger LOG;
+
+    @Autowired
+    public static String getJarDir() {
+        try {
+            // Retrieve the path as a URL and convert it to a URI
+            URI uri = QuartzSchedulerApplication.class.getProtectionDomain().getCodeSource().getLocation().toURI();
+
+            // Convert to a String and remove the prefix if it starts with "jar:"
+            String path = uri.toString();
+            if (path.startsWith("jar:")) {
+                path = path.substring(4); // Strip off "jar:"
+            }
+
+            // Locate the .jar segment and trim everything after it
+            int jarEndIndex = path.indexOf(".jar");
+            if (jarEndIndex != -1) {
+                path = path.substring(0, jarEndIndex + 4); // ".jar" has 4 characters
+            }
+
+            // Convert back to URI, then to Path, and get the absolute path
+            return Paths.get(new URI(path)).toAbsolutePath().toString();
+
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("Failed to determine the JAR file path.", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            // Determine the directory of the JAR file
+            String jarDir = getJarDir();
+            System.out.println("here " + jarDir);
+
+            // Specify the location for log4j2.xml
+            String log4jConfigFilePath = jarDir + File.separator + "log4j2.xml";
+
+            // Generate log4j2.xml based on job.properties
+            Properties jobProperties = JobPropertiesLoader.loadJobProperties(jarDir + File.separator + "job.properties");
+            Log4j2XmlGenerator.generateLog4j2Xml(jobProperties, log4jConfigFilePath);
+
+            // Set the log4j configuration file system property
+            System.setProperty("log4j.configurationFile", log4jConfigFilePath);
+            System.setProperty("log4j2.debug", "true");
+
+
+            ConfigurationSource source = new ConfigurationSource(new FileInputStream(log4jConfigFilePath));
+            Configurator.initialize(null, source);
+
+            LOG = LogManager.getLogger(QuartzSchedulerApplication.class);
+
+            SpringApplication.run(QuartzSchedulerApplication.class, args).getBean(QuartzSchedulerApplication.class).scheduleJobs();
+
+            //get data from DB
+            ApplicationContext context = SpringApplication.run(QuartzSchedulerApplication.class, args);
+            UserRepo repo = context.getBean(UserRepo.class);
+            System.out.println("repo: " + repo.findAll());
+
+        } catch (IOException e) {
+            LOG.debug(e);
+        }
+    }
+
+//    public static void main(String[] args) {
+//        // Specify the URL with integrated security
+//        String url = "jdbc:sqlserver://localhost:1433;databaseName=quartz_scheduler;encrypt=true;trustServerCertificate=true;integratedSecurity=true;";
 //
-//    public static String getJarDir() {
 //        try {
-//            // Get the location of the JAR file
-//            File jarFile = new File(QuartzSchedulerApplication.class.getProtectionDomain().getCodeSource().getLocation().toURI());
-//            // Get the parent directory of the JAR file
-//            return jarFile.getParentFile().getAbsolutePath();
-//        } catch (URISyntaxException e) {
-//            throw new RuntimeException("Failed to determine the JAR file directory.", e);
+//            ApplicationContext context = SpringApplication.run(QuartzSchedulerApplication.class, args);
+//
+//            User user1 = context.getBean(User.class);
+////            user1.setId(111);
+////            user1.setName("kei");
+////            user1.setGender("JAVA");
+////
+//            UserRepo repo = context.getBean(UserRepo.class);
+////            repo.save(user1);
+//
+//            System.out.println(repo.findAll());
+//
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
 //        }
 //    }
 
-    public static void main(String[] args) {
-        // Specify the URL with integrated security
-        String url = "jdbc:sqlserver://localhost:1433;databaseName=quartz_scheduler;encrypt=true;trustServerCertificate=true;integratedSecurity=true;";
+    public void scheduleJobs() {
+        try (InputStream externalInput = Files.newInputStream(Paths.get("job.properties"))) {
 
-        try {
-            ApplicationContext context = SpringApplication.run(QuartzSchedulerApplication.class, args);
+            Properties prop = new Properties();
+            prop.load(externalInput);
 
-            User user1 = context.getBean(User.class);
-            user1.setId(111);
-            user1.setName("kei");
-            user1.setGender("JAVA");
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
 
-            UserRepo repo = context.getBean(UserRepo.class);
-            repo.save(user1);
+            for (String jobName : prop.stringPropertyNames()) {
+                if (jobName.endsWith(".cron")) {
+                    String jobKey = jobName.substring(4, jobName.length() - 5); // Remove the ".cron" suffix
+                    String cronExp = prop.getProperty(jobName);
+                    String commandKey = "job." + jobKey + ".command";
+                    String commandValue = prop.getProperty(commandKey);
 
-            System.out.println(repo.findAll());
+                    LOG.info("Processing job: {}, cron: {}, command: {}", jobKey, cronExp, commandValue);
 
+                    scheduleJob(scheduler, jobKey, cronExp, commandValue);
+                }
+            }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+            LOG.info("Scheduled all jobs.");
+        } catch (IOException | SchedulerException ex) {
+            LOG.error("Error scheduling jobs", ex);
         }
+    }
+
+    private void scheduleJob(Scheduler scheduler, String jobKey, String cronExp, String commandValue) throws SchedulerException {
+        JobDetail jobDetail = JobBuilder.newJob(BatchJob.class)
+                .withIdentity(jobKey)
+                .usingJobData("command", commandValue)
+                .usingJobData("folder", jobKey)
+                .build();
+
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .withIdentity(jobKey + "_trigger")
+                .withSchedule(CronScheduleBuilder.cronSchedule(cronExp))
+                .build();
+
+        scheduler.scheduleJob(jobDetail, trigger);
     }
 
 //    public void scheduleJobs() {
